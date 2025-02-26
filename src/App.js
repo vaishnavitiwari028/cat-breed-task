33329b2003655367356173c5a7a816413b59b10d
vaishnavitiwari028 <vaishnavitiwari028@gmail.com>
2025-02-26 14:03:24 +0530

    cat-breed-task

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BreedListPage from './pages/BreedListPage';
+
+function App() {
+  return (
+    <div className="App">
+     <BreedListPage/>
+    </div>
+  );
+}
+
+export default App;
